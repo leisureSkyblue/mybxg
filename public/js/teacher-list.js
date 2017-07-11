32dfd01e2c48dd0bab435d4737a844b9bf2ef6aa
leisureSkyblue <734392810@qq.com>
2017-07-11 19:47:48 +0800

    完成讲师信息预览

--- public/js/teacher-list.js
+++ public/js/teacher-list.js
@@ -7,6 +7,23 @@
         success:function(data){
             var html = template('teacherTpl',{list:data.result});
             $("#teacherInfo").html(html);
+	     //讲师信息模态框渲染
+            $("#teacherInfo").find('.preview').click(function(){
+                var td = $(this).parent();
+                var tcId = td.attr('data-tcId');
+                //通过ajax请求获取讲师详细信息
+                $.ajax({
+                    type:'get',
+                    url:'/api/teacher/view',
+                    data:{tc_id:tcId},
+                    dataType:'json',
+                    success:function(data){
+                        var html = template('modalTpl',data.result);
+                        $("#modalInfo").html(html);
+                        $("#teacherModal").modal();
+                    }
+                });
+            });
         }
     });
 });
